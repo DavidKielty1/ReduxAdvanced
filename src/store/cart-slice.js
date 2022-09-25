efb613ad1d9ae1a29ed28842cca0963f3dd078f6
DavidKielty1 <davidkielty@live.co.uk>
2022-09-25 19:36:51 +0100

    Action creator fetchCartData. Deployed in useEffect within App, imported from cart-actions, requiring cartslice. Fetches data from firebase upon loadout.

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,6 +4,10 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [], totalQuantity: 0 },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -21,7 +25,6 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
